5270f75d0a96322b8f932f8e401c72e09c17ef6f
Jaswanth Akula <jaswanthakula121@gmail.com>
2026-04-14 12:40:24 +0530

    fix db ssl connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,12 +11,24 @@ import { ApplicationsModule } from './applications/applications.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
+
+      // Load entities automatically
       autoLoadEntities: true,
+
+      // Keep true for now (easier)
       synchronize: true,
+
+      // 🔥 VERY IMPORTANT (fix for Railway/Supabase)
       ssl: {
         rejectUnauthorized: false,
       },
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
     }),
+
     JobsModule,
     CandidatesModule,
     ApplicationsModule,
